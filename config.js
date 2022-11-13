216f8b833069ee0c5b6ddf1985d7353d75f588f3
Phúc Võ <98152897+Phuchello1@users.noreply.github.com>
2022-11-13 18:22:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Thoát ra không trả lời = đồng ý =))",
+    introTitle: 'Bạn trước màn hình :3',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
